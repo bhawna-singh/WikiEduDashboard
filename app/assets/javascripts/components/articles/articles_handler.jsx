ed5c67ff838e4088adef66ddbab295066361c3fb
Wes Reid <bwreid@gmail.com>
2019-03-08 15:52:27 -0800

    Add navigation for article tab
    - Swap between Edited, Assigned, and Available articles

--- app/assets/javascripts/components/articles/articles_handler.jsx
+++ app/assets/javascripts/components/articles/articles_handler.jsx
@@ -28,6 +28,14 @@ export const ArticlesHandler = createReactClass({
     loadingAssignments: PropTypes.bool
   },
 
+  getInitialState() {
+    return {
+      showArticlesEdited: true,
+      showAssignedArticles: false,
+      showAvailableArticles: false
+    };
+  },
+
   componentWillMount() {
     if (this.props.loadingAssignments) {
       this.props.fetchAssignments(this.props.course_id);
@@ -49,6 +57,16 @@ export const ArticlesHandler = createReactClass({
     return this.props.filterNewness(e.target.value);
   },
 
+  toggleSection(key) {
+    const state = {
+      showArticlesEdited: false,
+      showAssignedArticles: false,
+      showAvailableArticles: false
+    };
+
+    this.setState({ ...state, [key]: true });
+  },
+
   showMore() {
     return this.props.fetchArticles(this.props.course_id, this.props.limit + 500);
   },
@@ -62,11 +80,12 @@ export const ArticlesHandler = createReactClass({
     // mounted in the first place if they are not available.
     if (!this.props.course || !this.props.course.home_wiki) { return <div />; }
 
-    let showMoreButton;
-    if (!this.props.limitReached) {
-      showMoreButton = <div><button className="button ghost stacked right" onClick={this.showMore}>{I18n.t('articles.see_more')}</button></div>;
+    let categories;
+    if (this.props.course.type === 'ArticleScopedProgram') {
+      categories = <CategoryHandler course={this.props.course} current_user={this.props.current_user} />;
     }
 
+    // The following is all for the "Articles Edited" section
     let header;
     if (Features.wikiEd) {
       header = <h3 className="article tooltip-trigger">{I18n.t('metrics.articles_edited')}</h3>;
@@ -79,11 +98,6 @@ export const ArticlesHandler = createReactClass({
           </div>
         </h3>
       );
-    }
-
-    let categories;
-    if (this.props.course.type === 'ArticleScopedProgram') {
-      categories = <CategoryHandler course={this.props.course} current_user={this.props.current_user} />;
     }
 
     let filterWikis;
@@ -116,36 +130,94 @@ export const ArticlesHandler = createReactClass({
     if (!!filterWikis || !!filterArticlesSelect) {
       filterLabel = <b>Filters:</b>;
     }
+
+    const articleSort = (
+      <div className="article-sort">
+        <select className="sorts" name="sorts" onChange={this.sortSelect}>
+          <option value="rating_num">{I18n.t('articles.rating')}</option>
+          <option value="title">{I18n.t('articles.title')}</option>
+          <option value="character_sum">{I18n.t('metrics.char_added')}</option>
+          <option value="view_count">{I18n.t('metrics.view')}</option>
+        </select>
+      </div>
+    );
+
+    const sectionHeader = (
+      <div className="section-header">
+        {header}
+        <CourseOresPlot course={this.props.course} />
+        <div className="wrap-filters">
+          {filterLabel}
+          {filterArticlesSelect}
+          {filterWikis}
+          {articleSort}
+        </div>
+      </div>
+    );
+
+    let showMoreButton;
+    if (!this.props.limitReached) {
+      showMoreButton = <div><button className="button ghost stacked right" onClick={this.showMore}>{I18n.t('articles.see_more')}</button></div>;
+    }
+
     return (
-      <div>
-        <div id="articles">
-          <div className="section-header">
-            {header}
-            <CourseOresPlot course={this.props.course} />
-            <div className="wrap-filters">
-              {filterLabel}
-              {filterArticlesSelect}
-              {filterWikis}
-              <div className="article-sort">
-                <select className="sorts" name="sorts" onChange={this.sortSelect}>
-                  <option value="rating_num">{I18n.t('articles.rating')}</option>
-                  <option value="title">{I18n.t('articles.title')}</option>
-                  <option value="character_sum">{I18n.t('metrics.char_added')}</option>
-                  <option value="view_count">{I18n.t('metrics.view')}</option>
-                </select>
-              </div>
+      <div className="articles-view">
+        <nav>
+          <ul>
+            <li>
+              <button
+                className={`button ${this.state.showArticlesEdited ? 'active' : ''}`}
+                onClick={() => this.toggleSection('showArticlesEdited')}
+              >
+                Articles Edited
+              </button>
+            </li>
+            <li>
+              <button
+                className={`button ${this.state.showAssignedArticles ? 'active' : ''}`}
+                onClick={() => this.toggleSection('showAssignedArticles')}
+              >
+                Assigned Articles
+              </button>
+            </li>
+            <li>
+              <button
+                className={`button ${this.state.showAvailableArticles ? 'active' : ''}`}
+                onClick={() => this.toggleSection('showAvailableArticles')}
+              >
+                Available Articles
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+        {
+          this.state.showArticlesEdited && (
+            <div id="articles" className="mt4">
+              {sectionHeader}
+              <ArticleList {...this.props} articles={this.props.articles} sortBy={this.props.sortArticles} />
+              {showMoreButton}
             </div>
-          </div>
-          <ArticleList {...this.props} articles={this.props.articles} sortBy={this.props.sortArticles} />
-          {showMoreButton}
-        </div>
-        <div id="assignments" className="mt4">
-          <div className="section-header">
-            <h3>{I18n.t('articles.assigned')}</h3>
-          </div>
-          <AssignmentList {...this.props} />
-        </div>
-        <AvailableArticles {...this.props} />
+          )
+        }
+
+        {
+          this.state.showAssignedArticles && (
+            <div id="assignments" className="mt4">
+              <div className="section-header">
+                <h3>{I18n.t('articles.assigned')}</h3>
+              </div>
+              <AssignmentList {...this.props} />
+            </div>
+          )
+        }
+
+        {
+          this.state.showAvailableArticles && (
+            <AvailableArticles {...this.props} />
+          )
+        }
+
         {categories}
       </div>
     );
